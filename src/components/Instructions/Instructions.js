a311c9a4b20f92094f9382ba8f65b8b551a031f6
tomas-novak-git <tomas_novak@centrum.cz>
2024-03-01 17:16:05 +0100

    instructions add. final

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 function Instructions() {
   return <div className='instruction-box'>
+    <h3>You have 6 tries to find the right word!</h3>
     <div className='instruction-line'>
       <span className="instruct-span correct"></span>
       <p>={''} Letter and order is correct</p>
